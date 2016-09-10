4c6d293262b2fa30052b42ae0842e4f7c8fe51c6
koo2332 <koo2332@icloud.com>
2016-09-10 16:55:27 +0900

    my second project

--- Hellojava.java
+++ Hellojava.java
@@ -4,6 +4,8 @@ public class Hellojava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellojava");
+		System.out.println("HelloHello");
+
 
 	}
 
